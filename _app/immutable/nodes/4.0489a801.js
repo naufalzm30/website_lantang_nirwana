0f4df0d076b4c04194a5639df0dbfa72a7e57691
naufalzm30 <naufalzm30@gmail.com>
2023-09-26 00:06:25 +0700

    Updates

---
+++ _app/immutable/nodes/4.0489a801.js
@@ -0,0 +1,7 @@
+import{s as p,f as g,n as o}from"../chunks/scheduler.63274e7e.js";import{S as x,i as _,g as c,s as b,h as m,j as v,c as y,x as w,f as d,k as r,a as H,z as h}from"../chunks/index.ccb240dc.js";const L=""+new URL("../assets/foto_nga_ada.12986445.png",import.meta.url).href;function k(l){let e,t,u,i,a,f=`Hai, I’m Lantang Nirwana<br/>
+        a musician/programmer from Bandung.<br/>
+        Oh and yeah, i code this website by myself hahaha.<br/>
+        So please, explore this website and my social media<br/>
+        and dont forget to check out my songs!!!<br/> <br/> <br/> <br/>
+        with love, from me, to you.<br/>
+        (i know, its The Beatles song)`;return{c(){e=c("div"),t=c("img"),i=b(),a=c("p"),a.innerHTML=f,this.h()},l(n){e=m(n,"DIV",{class:!0});var s=v(e);t=m(s,"IMG",{src:!0,class:!0,alt:!0}),i=y(s),a=m(s,"P",{class:!0,"data-svelte-h":!0}),w(a)!=="svelte-f164yu"&&(a.innerHTML=f),s.forEach(d),this.h()},h(){g(t.src,u=l[0]["about.png"])||r(t,"src",u),r(t,"class","mt-14 align-middle max-w-[30%] md:mt-5"),r(t,"alt",""),r(a,"class","flex font-Helvetica text-putih-lantang xl:text-2xl"),r(e,"class","min-w-full flex justify-center items-center xl:gap-30 xl:flex-row flex-col gap-14")},m(n,s){H(n,e,s),h(e,t),h(e,i),h(e,a)},p:o,i:o,o,d(n){n&&d(e)}}}function I(l){return[{"about.png":L}]}class T extends x{constructor(e){super(),_(this,e,I,k,p,{})}}export{T as component};
